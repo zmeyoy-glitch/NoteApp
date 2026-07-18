cdda17dafd1292104513192903c2ac77dde103c0
zmeyoy-glitch <zmeyoy@gmail.com>
2026-07-18 23:44:14 +0300

    Root build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
-import org.jetbrains.kotlin.kapt3.gradleplugin.Kapt
-
 plugins {
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+    delete(rootProject.buildDir)
 }

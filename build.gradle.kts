ba31c02ed6fb4d4bc6507b1f66b925686b676ff3
zmeyoy-glitch <zmeyoy@gmail.com>
2026-07-18 23:46:54 +0300

    Root build configuration with Android and Kotlin plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
 tasks.register("clean", Delete::class) {

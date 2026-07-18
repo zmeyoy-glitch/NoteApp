dfc01713f29c6efe52cb7f00184b83319cf13286
zmeyoy-glitch <zmeyoy@gmail.com>
2026-07-19 01:30:03 +0300

    feat: Add ViewModelFactory for dependency injection of ViewModels

---
+++ app/src/main/java/com/example/noteapp/data/ViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.noteapp.data
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ViewModelFactory(
+    private val repository: NoteRepository
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NoteViewModel::class.java)) {
+            return NoteViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}

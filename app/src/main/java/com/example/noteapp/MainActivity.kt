78c5d7ef3bfae742dcaf39fd16b22bfd34627e65
zmeyoy-glitch <zmeyoy@gmail.com>
2026-07-19 01:37:50 +0300

    Create basic MainActivity skeleton file.

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,37 +1,8 @@
 package com.example.noteapp
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.noteapp.data.NoteDatabase
-import com.example.noteapp.data.NoteEntity
-import com.example.noteapp.data.NoteRepository
-import com.example.noteapp.data.NoteViewModel
-
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewModel: NoteViewModel
-    private lateinit var adapter: NoteAdapter
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        val database = NoteDatabase.getInstance(applicationContext)
-        val repository = NoteRepository(database.noteDao())
-        viewModel = ViewModelProvider(this, ViewModelFactory(repository))[NoteViewModel::class.java]
-
-        adapter = NoteAdapter()
-        recyclerView.adapter = adapter
-
-        viewModel.notes.observe(this) { notes ->
-            adapter.submitList(notes)
-        }
+class MainActivity {
+    // Main activity entry point
+    fun onCreate() {
+        println("Hello from MainActivity!")
     }
 }

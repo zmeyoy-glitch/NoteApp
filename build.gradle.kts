317352548b252e50913b73117166b119d1a43a5c
zmeyoy-glitch <zmeyoy@gmail.com>
2026-07-18 23:45:15 +0300

    Root build configuration with Android and Kotlin plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,8 @@
-import org.jetbrains.kotlin.konan.properties.Properties
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
-val localProperties = Properties()
-localProperties.load(java.io.FileInputStream("local.properties").bufferedReader())
-
-// Read version from properties file or use default values
-val appVersionName: String? = localProperties.getProperty("app.version.name") ?: "1.0.0"
-val appVersionCode: Int = localProperties.getProperty("app.version.code", "1").toInt()
-
-println("Building NoteApp v$appVersionName (code $appVersionCode)")
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
